ecde4cefb90d071ee5a23f4613454ff4ca48b14b
ritiks236 <112147362+ritiks236@users.noreply.github.com>
2023-04-03 23:40:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div clasNamw="w-[100vw] h-[100vh] flex items-ce">
+      <div>Increment & Decrement</div>
+      <div>
+        <button>-</button>
+        <div> </div>
+        <button>+</button>
+      </div>
+      <button>Reset</button>
+    </div>
+  );
+}
+
+export default App;
